3b0d15dff70b1b7bcb5c153460ac0cc9241cdc1f
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2024-07-16 20:07:35 -0400

    fix: trying to fix weekly report

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,30 @@
 <script lang="ts" setup>
+import { computed, onMounted, ref } from "vue";
+import { useRouter } from "vue-router";
 import Button from "../components/Button.vue";
 import AWS from "../components/icons/AWS.vue";
-import { generateWeeklyReport } from "../lib/analytics";
+import Spinner from "../components/icons/Spinner.vue";
+import { generateWeeklyReport, type WeeklyReport } from "../lib/analytics";
 import { getRandomQuestionURL, questions } from "../lib/questions";
 
-const reports = generateWeeklyReport();
-const todayReport = reports[0];
+const router = useRouter();
 
 const weekDays: Record<number, string> = {
-  0: "Domingo",
-  1: "Segunda",
-  2: "Terça",
-  3: "Quarta",
-  4: "Quinta",
-  5: "Sexta",
-  6: "Sábado",
+  0: "Dom",
+  1: "Seg",
+  2: "Ter",
+  3: "Qua",
+  4: "Qui",
+  5: "Sex",
+  6: "Sáb",
 };
+
+const reports = ref<WeeklyReport[] | null>(null);
+const todayReport = computed(() => reports.value?.[0]);
+
+onMounted(() => {
+  reports.value = generateWeeklyReport();
+});
 </script>
 
 <template>
@@ -29,56 +38,60 @@ const weekDays: Record<number, string> = {
         Teste seus conhecimentos em AWS com cerca de
         {{ questions.length }} questões.
       </p>
-      <Button>
-        <RouterLink :to="getRandomQuestionURL()"> Começar </RouterLink>
+      <Button @click.prevent="router.replace({ path: getRandomQuestionURL() })">
+        Modo Zen
       </Button>
     </div>
     <div
       class="flex flex-col items-center w-full gap-3 p-6 rounded-lg bg-slate-800"
     >
-      <h2 class="text-2xl font-bold">Relatório</h2>
-      <div class="flex items-center gap-10">
-        <p class="text-lg font-medium text-slate-400">
-          {{
-            todayReport.correctQuestions.count +
-            todayReport.wrongQuestions.count
-          }}
-          questões hoje.
-        </p>
-        <p class="text-lg font-medium text-slate-400">
-          {{ todayReport.correctQuestions.percentage }}% de acerto ({{
-            todayReport.correctQuestions.count
-          }})
-        </p>
-      </div>
-      <div class="flex items-center justify-between gap-3">
+      <template v-if="reports && todayReport">
+        <h2 class="text-2xl font-bold">Relatório</h2>
         <div
-          :key="report.date.getTime()"
-          v-for="[index, report] of reports.entries()"
-          class="relative w-10 h-32 rounded-lg bg-slate-900"
+          class="flex items-center gap-10 text-sm font-medium text-center sm:text-lg text-slate-400"
         >
-          <div
-            class="absolute bottom-0 left-0 right-0 bg-green-500 rounded-lg"
-            :style="{ height: `${report.correctQuestions.percentage}%` }"
-          />
-          <span
-            class="absolute text-sm font-bold -translate-x-1/2 bottom-1 left-1/2 text-slate-800"
-            >{{ report.correctQuestions.percentage }}%</span
-          >
-          <p
-            class="font-medium absolute top-[calc(100%+44px)] left-0 rotate-45 text-start w-[120px]"
-          >
+          <p>
             {{
-              index === 0
-                ? "Hoje"
-                : index === 1
-                ? "Ontem"
-                : weekDays[report.date.getDay()]
+              todayReport.correctQuestions.count +
+              todayReport.wrongQuestions.count
             }}
-            ({{ report.totalCount }})
+            questões hoje.
+          </p>
+          <p>
+            {{ todayReport.correctQuestions.percentage }}% de acerto ({{
+              todayReport.correctQuestions.count
+            }})
           </p>
         </div>
-      </div>
+        <div
+          class="grid items-center justify-between w-full grid-cols-7 gap-1 mb-4"
+        >
+          <div
+            :key="report.date.getTime()"
+            v-for="[index, report] of reports.entries()"
+            class="relative w-full h-32 rounded-lg bg-slate-900"
+          >
+            <div
+              class="absolute bottom-0 left-0 right-0 bg-green-500 rounded-lg"
+              :style="{ height: `${report.correctQuestions.percentage}%` }"
+            />
+            <span
+              class="absolute text-xs font-bold -translate-x-1/2 bottom-1 left-1/2 text-slate-800 sm:text-sm"
+            >
+              {{ report.correctQuestions.percentage }}%
+            </span>
+            <p
+              class="font-medium absolute top-[calc(100%+2px)] text-slate-400 left-1/2 -translate-x-1/2 text-xs w-full text-center font-mono"
+            >
+              {{ index === 0 ? "Hoje" : weekDays[report.date.getDay()] }}
+              ({{ report.totalCount }})
+            </p>
+          </div>
+        </div>
+      </template>
+      <template v-else>
+        <Spinner width="64" height="64" />
+      </template>
     </div>
   </main>
 </template>

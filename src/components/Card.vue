e90590b97a6be55e82d4dd8043cf258c2b54b811
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2024-07-17 01:39:10 -0400

    feat: add card component

---
+++ src/components/Card.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+defineOptions({
+  name: "Card",
+});
+</script>
+
+<template>
+  <div class="flex flex-col gap-3 p-6 rounded-lg bg-slate-800">
+    <slot />
+  </div>
+</template>
